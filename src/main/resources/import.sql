091986571be3360f29626db8b0d5dedd93233c19
Fabricio Pita <fabricio001400@gmail.com>
2023-06-28 22:12:56 -0300

    Course, Offer

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -12,3 +12,8 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
+
+INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML', 'https://pxhere.com/en/photo/1443423', 'https://pxhere.com/en/photo/1620283');
+
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2023-11-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2024-11-20T03:00:00Z', 1);
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2023-12-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2024-12-20T03:00:00Z', 1);
