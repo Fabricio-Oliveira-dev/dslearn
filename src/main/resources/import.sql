80c33bcd99db4fde0213cb183f4c55884ec55974
Fabricio Pita <fabricio001400@gmail.com>
2023-07-06 19:43:33 -0300

    Section

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -19,5 +19,9 @@ INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0
 INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2023-12-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2024-12-20T03:00:00Z', 1);
 
 INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Trilha HTML', 'Trilha principal do curso', 1, 'https://pxhere.com/en/photo/1443423', 1, 1);
-INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Forum', 'Tire suad dúvidas', 2, 'https://pxhere.com/en/photo/1443423', 2, 1);
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Forum', 'Tire suas dúvidas', 2, 'https://pxhere.com/en/photo/1443423', 2, 1);
 INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Lives', 'Lives exclusivas', 3, 'https://pxhere.com/en/photo/1443423', 0, 1);
+
+INSERT INTO tb_section(title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capítulo 1', 'Neste capítulo será o básico', 1, 'https://pxhere.com/en/photo/1443423', 1, null);
+INSERT INTO tb_section(title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capítulo 2', 'Neste capítulo será o intermediário', 2, 'https://pxhere.com/en/photo/1443423', 1, 1);
+INSERT INTO tb_section(title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capítulo 3', 'Neste capítulo será o avançado', 3, 'https://pxhere.com/en/photo/1443423', 1, 2);
