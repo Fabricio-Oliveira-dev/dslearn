a57e70a8316d3391c4224cf0e2034af348923810
Fabricio Pita <fabricio001400@gmail.com>
2023-07-06 20:51:30 -0300

    Enrollment

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -25,3 +25,6 @@ INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) 
 INSERT INTO tb_section(title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capítulo 1', 'Neste capítulo será o básico', 1, 'https://pxhere.com/en/photo/1443423', 1, null);
 INSERT INTO tb_section(title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capítulo 2', 'Neste capítulo será o intermediário', 2, 'https://pxhere.com/en/photo/1443423', 1, 1);
 INSERT INTO tb_section(title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capítulo 3', 'Neste capítulo será o avançado', 3, 'https://pxhere.com/en/photo/1443423', 1, 2);
+
+INSERT INTO tb_enrollment(user_id, offer_id, enroll_Moment, refund_Moment, available, only_Update) VALUES (1, 1, TIMESTAMP WITH TIME ZONE '2023-11-20T13:00:00Z', null, true, false);
+INSERT INTO tb_enrollment(user_id, offer_id, enroll_Moment, refund_Moment, available, only_Update) VALUES (2, 1, TIMESTAMP WITH TIME ZONE '2023-11-20T13:00:00Z', null, true, false);
