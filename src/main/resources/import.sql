902452bfbbaaf67f820cc8bfa91b29c1c44d5f2d
Fabricio Pita <fabricio001400@gmail.com>
2023-07-06 19:22:55 -0300

    Resource

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -17,3 +17,7 @@ INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML', 'ht
 
 INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2023-11-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2024-11-20T03:00:00Z', 1);
 INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2023-12-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2024-12-20T03:00:00Z', 1);
+
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Trilha HTML', 'Trilha principal do curso', 1, 'https://pxhere.com/en/photo/1443423', 1, 1);
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Forum', 'Tire suad dúvidas', 2, 'https://pxhere.com/en/photo/1443423', 2, 1);
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Lives', 'Lives exclusivas', 3, 'https://pxhere.com/en/photo/1443423', 0, 1);
